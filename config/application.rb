95e92c648424f5dc1cac6f0ffb8c12dab1d3724d
David M <davidmh.mail@gmail.com>
2017-04-17 18:37:58 -0600

    User Story 1: User Sign Up - completed

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,16 @@ module Blocipedia
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+		ActionMailer::Base.smtp_settings = {
+			address: 'smtp.gmail.com',
+			domain: 'mail.google.com',
+			port: 587,
+			user_name: 'appmailerservice@gmail.com',
+			password: ENV['APPMAILERSERVICE_GMAIL_PASSWORD'],
+			authentication: 'login',
+			enable_starttls_auto: true
+		}
+		
   end
 end

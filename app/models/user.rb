6c120e22e6354cb3d568fa9639c16d320a082b91
David M <davidmh.mail@gmail.com>
2017-04-21 19:35:25 -0600

    User Story 4: User Roles - Implement user roles enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
 	before_save { self.role ||= :standard}
 
 	validates :name, length: {minimum: 1, maximum: 100}, presence: true
+
+	enum role: [:standard, :premium, :admin]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,

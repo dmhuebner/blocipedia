8929b58a765716052a20d5dbf97473b125140581
David M <davidmh.mail@gmail.com>
2017-04-25 22:36:47 -0600

    User Story 9: Wiki Collaborators - create migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :wikis, dependent: :destroy
+	has_many :collaborators
+	# has_many :wikis, through: :collaborators
 
 	before_save { self.role ||= :standard}
 
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,5 +1,7 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
+	has_many :collaborators
+	has_many :users, through: :collaborators
 
 	default_scope { order('updated_at DESC') }
 
